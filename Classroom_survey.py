9fe02208d41a87019c7351ae2be06ee525ab6d8b
Nhlonhlorhi <mabasanhlonhlorhi@gmail.com>
2025-06-02 14:15:57 +0200

    update

---
+++ Classroom_survey.py
@@ -0,0 +1,18 @@
+questions = [
+    "Do you like Python? (yes/no)",
+    "Have you used input() before? (yes/no)",
+    "Do you enjoy group projects? (yes/no)",
+    "Would you like to learn more about dictionaries? (yes/no)",
+    "Is programming fun for you? (yes/no)"
+]
+
+def collect_responses(questions, num_participants):
+    all_responses = []
+    for i in range (num_participants):
+        print(f"\nParticipants {i + 1}, please anser the following questions:")
+        participant_answers = []
+        for q in questions :
+            answer = input (q +" ").strip().lower()
+            participant_answers.append(answer)
+            all_responses.append(participant_answers)
+            return all_responses
